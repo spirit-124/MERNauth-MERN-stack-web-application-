83652e7d8fa2cbf7931bb3e43652df60f2db108a
spirit-124 <bajrangofficial580@gmail.com>
2023-05-20 20:10:54 +0530

    [Setuped] getprofile and udateProfile Routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -63,14 +63,49 @@ const logoutUser = asyncHander(async (req, res) => {
 // route GET /api/user/profile
 // @access private
 const getUserProfile = asyncHander(async (req, res) => {
-  res.status(200).json({ message: " User Profile" });
+  const user = await User.findById(req.user._id);
+  // const user = {
+  //   _id: user._id,
+  //   email: user.email,
+  //   name: user.name,
+  // };
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 // @desc Update User Profile
 // route PUT /api/user/profile
 // @access private
 const updateUserProfile = asyncHander(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+    // console.log(updatedUser._id, updatedUser.name, updatedUser.email);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
